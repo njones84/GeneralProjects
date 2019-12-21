061437d18612bc1235359d374258e24b0fb8d1d9
njones84 <46726972+njones84@users.noreply.github.com>
2019-12-21 15:18:54 -0500

    Add files via upload

---
+++ FormationalMovement/GridManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    private NodeManager[,] grid;
+    private int nodeSize;
+    private int rows;
+    private int cols;
+
+    void Start()
+    {
+        nodeSize = 1;
+        rows = 50;
+        cols = 50;
+
+        GenerateGrid();
+        DecomposeWorld();
+    }
+
+    public void GenerateGrid()
+    {
+        grid = new NodeManager[rows, cols];
+
+        for (int row = 0; row < rows; row++)
+            for (int col = 0; col < cols; col++)
+                grid[row, col] = new NodeManager(row, col, true, null);
+    }
+
+    public void DecomposeWorld()
+    {
+        float startX = 0;
+        float startZ = 0;
+        float nodeCenterOffset = nodeSize / 2f;
+
+        for (int row = 0; row < rows; row++)
+            for (int col = 0; col < cols; col++)
+            {
+                float x = startX + nodeCenterOffset + (nodeSize * row);
+                float z = startZ + nodeCenterOffset + (nodeSize * col);
+
+                Vector3 startPos = new Vector3(x, 20f, z);
+
+                // Does our raycast hit anything at this point in the map
+                RaycastHit hit;
+
+                // Bit shift the index of the layer (8) to get a bit mask
+                int layerMask = 1 << 8;
+
+                // This would cast rays only against colliders in layer 8.
+                // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+                layerMask = ~layerMask;
+
+                // Does the ray intersect any objects excluding the player layer
+                if (Physics.Raycast(startPos, Vector3.down, out hit, Mathf.Infinity, layerMask))
+                {
+                    Debug.DrawRay(startPos, Vector3.down * 20, Color.red, 50000);
+                    grid[row, col].SetNavigable(false);
+                    Debug.Log("Decomposing [" + row + ", " + col + "]: " + grid[row, col].GetNavigable());
+                }
+                else
+                {
+                    // Debug.DrawRay(startPos, Vector3.down * 20, Color.green, 50000);
+                    grid[col, row].SetNavigable(true);
+                }
+            }
+    }
+    public void SetNodesToNull()
+    {
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                grid[i, j].SetParent(null);
+    }
+
+    public List<NodeManager> GetNeighbors(NodeManager node)
+    {
+        List<NodeManager> neighbors = new List<NodeManager>();
+
+        // access and store x and z position for neighbor finding
+        int x = node.GetXPosition();
+        int z = node.GetZPosition();
+
+        // check to see if out of bounds, if not check to see if it is navigable, if not add it to the neighbors list
+        // down
+        if (x + 1 < rows)
+            if (grid[x + 1, z].GetNavigable())
+                neighbors.Add(grid[x + 1, z]);
+
+        // up
+        if (x - 1 >= 0)
+            if (grid[x - 1, z].GetNavigable())
+                neighbors.Add(grid[x - 1, z]);
+
+        // left
+        if (z - 1 >= 0)
+            if (grid[x, z - 1].GetNavigable())
+                neighbors.Add(grid[x, z - 1]);
+
+        // right
+        if (z + 1 < cols)
+            if (grid[x, z + 1].GetNavigable())
+                neighbors.Add(grid[x, z + 1]);
+
+        // up-right
+        if (x - 1 >= 0 && z + 1 < cols)
+            if (grid[x - 1, z + 1].GetNavigable())
+                neighbors.Add(grid[x - 1, z + 1]);
+
+        // up-left
+        if (x - 1 >= 0 && z - 1 >= 0)
+            if (grid[x - 1, z - 1].GetNavigable())
+                neighbors.Add(grid[x - 1, z - 1]);
+
+        // down-left
+        if (x + 1 < rows && z - 1 >= 0)
+            if (grid[x + 1, z - 1].GetNavigable())
+                neighbors.Add(grid[x + 1, z - 1]);
+
+        // down-right
+        if (x + 1 < rows && z + 1 < cols)
+            if (grid[x + 1, z + 1].GetNavigable())
+                neighbors.Add(grid[x + 1, z + 1]);
+
+        return neighbors;
+    }
+
+    public NodeManager GetNode(int x, int z) { return grid[x, z]; }
+    public int GetNodeSize() { return nodeSize; }
+}
---
+++ FormationalMovement/PlayerMovement.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private Transform trans;
+    [SerializeField] private Rigidbody rb;
+
+    private float movementSpeed;
+    private float radiusOfSat;
+    private float obstacleDodgeSpeed;
+    private bool arrived;
+
+    private Vector3 obstaclePosition;
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;
+    private Pathfinder pathfinder;
+    private Stack<Vector3> path;
+
+    void Start()
+    {
+        pathfinder = gameObject.GetComponent<Pathfinder>();
+        path = new Stack<Vector3>();
+
+        movementSpeed = 5f;
+        radiusOfSat = 2f;
+        obstacleDodgeSpeed = 1f;
+        arrived = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            GetWorldPoint();
+
+        if (Input.GetMouseButtonDown(1))
+            CreateObject();
+
+        if (!arrived)
+            MovePlayer(); 
+    }
+
+    void GetWorldPoint()
+    {
+        // clear path when selecting a new world position
+        path.Clear();
+
+        // shoot a ray from your screen to the world
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // if the ray hits something, set the target we want to move to
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPosition = new Vector3(hit.point.x, trans.position.y, hit.point.z);
+
+            // call A* to get optimal path (returns a list of nodes to traverse) (returns correct list of Vector3 coordinates)
+            path = pathfinder.AStar(trans.position, targetPosition);
+
+            // get next destination if path is not null
+            if (path != null)
+            {
+                // show all nodes needed to be traversed to reach goal in console
+                pathfinder.ShowNodes(path);
+
+                targetPosition = path.Pop();
+                arrived = false;
+                Debug.Log("Leaders 1st destination: " + targetPosition + ", Remaining path length: " + path.Count);
+            }
+        }
+    }
+    void CreateObject()
+    {
+        // shoot a ray from your screen to the world
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // if the ray hits something, set the target we want to create an object at
+        if (Physics.Raycast(ray, out hit))
+        {
+            // get the obstacles creation position and floor the position to set it in the middle of the node
+            obstaclePosition = new Vector3(((int) hit.point.x) + 0.5f, 1, ((int) hit.point.z) + 0.5f);
+
+            // create a new primitive object and set the position to the ray cast point
+            GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            cylinder.transform.position = obstaclePosition;
+            cylinder.layer = 0;
+            cylinder.transform.localScale = new Vector3(.5f, 1f, .5f);
+        }
+    }
+    void MovePlayer()
+    {
+        // if path is empty, do nothing
+        if (path.Count <= 0 || path == null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            arrived = true;
+        }
+
+        // get direction vector based on the targetPosition vector
+        Vector3 direction = targetPosition - trans.position;
+
+        // rotate player
+        targetRotation = Quaternion.LookRotation(-direction); // putting just "direction" here caused it to move opposite
+        trans.rotation = Quaternion.Lerp(trans.rotation, targetRotation, Time.deltaTime * movementSpeed);
+
+        // go towards current destination
+        rb.velocity = direction * 0.5f;
+
+        // if we are inside the radius of satisfaction, then change destinations
+        if (direction.magnitude < radiusOfSat)
+        {
+            targetPosition = path.Pop();
+            Debug.Log("Leaders next destination: " + targetPosition + ", Remaining path length: " + path.Count);
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name.Equals("Cylinder"))
+        {
+            Vector3 toObstacle = collision.gameObject.transform.position - trans.position;
+            toObstacle.Normalize();
+            toObstacle.y = 0f;
+
+            float dot = Vector3.Dot(trans.right, toObstacle);
+
+            // just rotate around the damn cylinder (orbit it until you ain't touching it I guess)
+            if (dot < 0f) // If I'm closer to the left side, go left
+            {
+                // Debug.Log("I'm on the right side.");
+                transform.RotateAround(collision.gameObject.transform.position, new Vector3(0f, 1f, 0f), obstacleDodgeSpeed);
+            }
+            else // If I'm closer to the right side, go right
+            {
+                // Debug.Log("I'm on the left side.");
+                transform.RotateAround(collision.gameObject.transform.position, new Vector3(0f, -1f, 0f), obstacleDodgeSpeed);
+            }
+
+            //Debug.Log("Velocity: " + rb.velocity);
+        }
+    }
+}
